baa60ef3a1098522758bb4d6dbd0e5edd925cf4e
mbuala <mbualayoka5@gmail.com>
2021-08-17 01:14:49 +0100

    ires

--- IresBox.sh
+++ IresBox.sh
@@ -2,8 +2,8 @@
 
 
 function main(){
-	dirIn="";
-	dirWork="";
+	dirIn="IN";
+	dirWork="WORK";
 	dirErreur1="";
 	dirErreur2="";
 	dirErreur3="";
@@ -11,11 +11,25 @@ function main(){
 	dirErreur5="";
 	dirArchives="";
 	dirConserves="";
-	dirLogs="";
-	dirCalques="";
-	calqueEntete="": #récupérer de la bdd
-	calquePiedPage="";
-	calqueSignature="";
+	dirLogs="LOGS";
+	dirCalques="CALQUES";
+
+	
+	DATABASE="iresbox"
+	USERNAME="mbuala"
+	HOsTNAME="localhost"
+	PORT="5432"
+	export PGPASSWORD=pg_db_password
+
+
+	#récupération des parametres dans la base de donnée
+
+	calqueEntete=$(psql -t -U $USERNAME -d $DATABASE -c "SELECT path FROM entetepage;") 
+	 echo $calqueEntete
+	calquePiedPage=$(psql -t -U $USERNAME -d $DATABASE -c "SELECT path FROM piedpage;") 
+	 echo $calquePiedPage
+	calqueSignature=$(psql -t -U $USERNAME -d $DATABASE -c "SELECT path FROM signature;") 
+	 echo $calqueSignature
 	dateProg="$(date +%d-%m-%Y' '%H:%M:%S)";
 	nomFileLog="LOG_"$nomSource"_"$(date +%d_%m_%Y);
     pile=10; #Nombre de fichiers a traiter par passage
@@ -25,14 +39,15 @@ function main(){
 	nbFileWork=`find $dirWork -type f -iname "*.pdf" | wc -l`;
 
 	#Generation fichier log
-	creatLogFile;
+	#creatLogFile;
 
 
 	if [ $nbFileWork != 0 ]; then
 
-        #TO DO
+      echo "work"
 	elif [ $nbFileIn != 0 ]; then
 		traitementPdf;
+		
 	fi;
 
 
@@ -46,18 +61,14 @@ function main(){
 			done;
 			filesWork=`ls $dirWork/*.pdf`;
 	        nbFileIn=`find $dirIn -type f -iname "*.pdf" | wc -l`;
-		    for file in $filesWork; do
-			    
+		    for file in $filesWork; do	   
 
- 
-
-			    ;
+			 	extractionData ;  
 			done;
 
 	        if [ $nbFileIn != 0 ]; then
-	            traitementPdf;
-	        fi;
-
+				traitementPdf;
+			fi;	
 		else
 			statutApp="Répertoire IN vide";
 			echo $dateProg": "$statutApp >> $dirLogs/$dateRep/$nomFileLog.txt;
@@ -78,45 +89,50 @@ function main(){
 		bioValideur="";
 
 		#Conversion pdftotext
+		echo "extractionData"
+		pdftotext $dirWork/$nameFile.pdf
 		if [ -e $dirWork/$nameFile.txt ]; then
 			#extraire les données
-			nomPatient= extractPatient; #utilisé les commande d'extraction grep -E
-			adressePatient= extractAdressePatient:
+			#utilisé les commande d'extraction grep -E
+			nomPatient="test" #extractPatient; 
+			adressePatient= extractAdressePatient;
 			sexe= extractSexePatient;
 			dateDeNaissance= extractDateDeNaissance;
 			emailPatient= extractEmailPatient;
 			emailMedecin= extractEmailMedecin;
 			medecin= extractMedecin;
 			bioValideur= extractBioValideur;
+			numeroDossier="12" #extractNumDossier;
+			if [ -z $numeroDossier ] && [ -e $nomPatient ] ; then  #Si les champs importants sont renseignés
 
-			if [ ];then  #Si les champs importants sont renseignés
-
-				if [ -z "$calqueEntete" ] && [ -e $dirWork/$nameFile.pdf ];then
-					ajoutCalque($calqueEntete):
-
+				if [ -z "$"calqueEntete"" ] && [ -e $dirWork/$nameFile.pdf ];then
+					ajoutCalque calqueEntete;
+					echo "calqueEntete"
 				fi;
 
-				if [ -z "$calquePiedPage" ] && [ -e $dirWork/$nameFile.pdf ];then
-					ajoutCalque($calquePiedPage):					
-
+				if [ -z "$"calquePiedPage"" ] && [ -e $dirWork/$nameFile.pdf ];then
+					ajoutCalque calquePiedPage;					
+					echo "calquePiedPage"
 				fi;
 
-				if [ -z "$calqueSignature" ] && [ -e $dirWork/$nameFile.pdf ];then
-					ajoutCalque($calqueSignature):					
-
+				if [ -z "$"calqueSignature"" ] && [ -e $dirWork/$nameFile.pdf ];then
+					ajoutCalque calqueSignature;					
+					echo "calqueSignature"
 				fi;
 
 				if [ -e $dirWork/$nameFile.pdf ];then
-					insertionData;
+					#insertionData;
+					echo "insertionData"
 				fi;
 
 			else
 				#ERREUR2
+				echo "ERREUR2"
 			fi;
 
 		else 
 			#ERREUR1
-
+			echo "ERREUR1"
 		fi;
 
 
@@ -124,24 +140,28 @@ function main(){
 
 	function insertionData(){
 		#rqt sql
-		rq_insertCRR=$(psql iresbox -c "INSERT INTO ")
+		rq_insertCRR=$(psql iresbox -U "mbuala" -c "INSERT INTO ")
 		if [ "$rq_insertCRR" = "INSERT 0 1" ]; then
-
+			echo "insertion avec succès"
 		else
 			statut=$nameFile"-> Erreur insertion données";
 			echo $dateProg": "$statut >> $dirLogs/$dateRep/$nomFileLog.txt
 		fi;
 	}
-
-	function ajoutCalque(calque){
+	#1
+	function ajoutCalque(){
 		#pdftk
-		if [ ]; then #si calque bien ajouter
-
+		arg1=$1 #$1 represente first argument
+		if [ -z $arg1 ]; then #si calque bien ajouter
+			echo "----"$arg1
 		else
 			#ERREUR3
+			echo "ERREUR3"
 		fi;
 	}
-
+	extractNumDossier(){
+		echo ""
+	}
 	function extractPatient(){
 		resultat=""
 		echo "$resultat"
@@ -155,6 +175,8 @@ function main(){
 	function extractSexePatient(){
 		resultat=""
 		echo "$resultat"
+		#resultat="" #$(grep -E "^Sexe" test.txt | awk {'print $2'})
+		#echo "$resultat"
 	}
 	function extractDateDeNaissance(){
 		resultat=""
@@ -175,20 +197,20 @@ function main(){
 	function extractBioValideur(){
 		resultat=""
 		echo "$resultat"
-	}
-
+	}	
 	function creatLogFile(){
-	#Verifier/creation si fichier LOG du jour exist
-	if [ ! -d $dirLogs ];then
-		mkdir $dirLogs;
-	fi;
-	if [ ! -d $dirLogs/$dateRep ];then
-		mkdir $dirLogs/$dateRep;
-	fi;
-	if [ ! -e "$dirLogs/$dateRep/$nomFileLog.txt" ];then
-		echo "---------- Traitement CRR ----------" >> $dirLogs/$dateRep/$nomFileLog.txt;
-	fi;
-}
+		#Verifier/creation si fichier LOG du jour exist
+		if [ ! -d $dirLogs ];then
+			chmod -R 777 mkdir $dirLogs;
+		fi;
+		if [ ! -d $dirLogs/$dateRep ];then
+			chmod -R 777 mkdir $dirLogs/$dateRep;
+		fi;
+		if [ ! -e "$dirLogs/$dateRep/$nomFileLog.txt" ];then
+			echo "---------- Traitement CRR ----------" >> $dirLogs/$dateRep/$nomFileLog.txt;
+		fi;
+		
+	}
 
 
 main;
