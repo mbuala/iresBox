addc72425f829f88914b3e7c3888a864e0bc5255
mbuala <mbualayoka5@gmail.com>
2021-08-23 13:27:10 +0100

    erreur data

--- IresBox.sh
+++ IresBox.sh
@@ -2,6 +2,15 @@
 
 
 		function main(){
+		#DATABASE PARAMETRES
+		DATABASE="iresbox"
+		USERNAME="mbuala"
+		HOsTNAME="localhost"
+		PORT="5432"
+		export PGPASSWORD=pg_db_password
+
+
+		#DOSSIERS 
 		dirIn="IN";
 		dirWork="WORK";
 		dirErreur1="ERREURS/ERREUR1";
@@ -14,23 +23,17 @@
 		dirLogs="LOGS";
 		dirCalques="CALQUES";
 
-
-		DATABASE="iresbox"
-		USERNAME="mbuala"
-		HOsTNAME="localhost"
-		PORT="5432"
-		export PGPASSWORD=pg_db_password
-
-
 		#récupération des parametres dans la base de donnée
 
 		calqueEntete=$(selectData "SELECT path FROM entetepage;")
 		calquePiedPage=$(selectData  "SELECT path FROM piedpage;") 
 		calqueSignature=$(selectData  "SELECT path FROM signature;") 
+
 		dateProg="$(date +%d-%m-%Y' '%H:%M:%S)";
 		dateRep="$(date +%d-%m-%Y)";
 		nomFileLog="LOG_"$nomSource"_"$(date +%d_%m_%Y);
 		pile=10; #Nombre de fichiers a traiter par passage
+		dateInsrt="$(date +%Y-%m-%d' '%H:%M:%S)";	
 
 		#Fichiers PDF
 		nbFileIn=`find $dirIn -type f -iname "*.pdf" | wc -l`;
@@ -96,18 +99,21 @@
 			if [ -e $dirWork/$nameFile.txt ]; then
 				#extraire les données
 				#utilisé les commande d'extraction grep -E
-				nomPatient="test" #extractPatient; 
-				adressePatient="marrakech" #extractAdressePatient;
-				sexe="m" #extractSexePatient;
-				dateDeNaissance="1992-08-11" #extractDateDeNaissance;
-				emailPatient="test@test.com" #extractEmailPatient;
-				emailMedecin="medecin@test.com" #extractEmailMedecin;
-				medecin="medecin" #extractMedecin;
-				bioValideur="bio" #extractBioValideur;
-				numeroDossier="14" #extractNumDossier;
-				datecreation=""
+				#EXTRACTION DES DONNÉES DANS LE FICHIER TXT
+				numeroDossier=$(grep -E "^Dossier N°" $dirWork/$nameFile.txt | awk {'print $3'})
+				nomPatient=$(grep -E "^Mme" $dirWork/$nameFile.txt | awk {'print  $2" "$3 '})
+				sexe=$(grep -E "^Sexe" $dirWork/$nameFile.txt | awk {'print $2'})
 				nbPage=$(pdfinfo  $dirWork/$nameFile.pdf | grep Pages | awk '{print $2}');
-				if [ -n "$numeroDossier" ] && [ -n "$nomPatient!" ] ; then  #Si les champs importants sont renseignés
+
+				adressePatient="marrakech" 				
+				dateDeNaissance="1992-08-11" 
+				emailPatient="test@test.com" 
+				emailMedecin="medecin@test.com"
+				medecin="medecin" 
+				bioValideur="bio"
+				datecreation=""
+				
+				if [ -n "$numeroDossier" ] && [ -n "$nomPatient" ] ; then  #Si les champs importants sont renseignés
 
 					if [ -n "$calqueEntete" ] && [ -e $dirWork/$nameFile.pdf ];then
 						ajoutCalque "$calqueEntete";
@@ -124,17 +130,8 @@
 					fi;
 
 					if [ -e $dirWork/$nameFile.pdf ];then
-								dateInsrt="$(date +%Y-%m-%d' '%H:%M:%S)";	
-								echo "insertionData"
-								#si le fichier est deja ajouté dans la base de donnée: deplacement dans  archive
-								findnumdossier=$(selectData "SELECT numerodossier FROM compterendubox WHERE numerodossier='$numeroDossier';")
 
-								if [ -n "$findnumdossier" ]; then
-									echo "file found in the DATABASE"
-									mv $dirWork/$nameFile.pdf $dirWork/$nameFile.txt $dirConserves/$dateRep
-									
-								
-								elif [ -n "$calqueEntete" ] || [ -n "$calqueSignature" ] || [ -n "$calquePiedPage" ]; then
+								if [ -n "$calqueEntete" ] || [ -n "$calqueSignature" ] || [ -n "$calquePiedPage" ]; then
 									
 									mv $dirWork/$nameFile.pdf $dirWork/$nameFile.txt $dirConserves/$dateRep
 									
@@ -142,15 +139,18 @@
 									('$dirArchives/$dateRep/$nameFile.pdf','$dateInsrt','$bioValideur','$nbPage',true,false,false,true,false,'$nomPatient','$numeroDossier','$sexe','$adressePatient','$medecin','$emailPatient');"
 								else
 									echo "pas de calques"
-									cp $dirWork/$nameFile.pdf $dirWork/$nameFile.txt  $dirConserves/$dateRep;	
-
 									cp $dirWork/$nameFile.pdf $dirArchives/$dateRep;	
-									#rm $dirWork/$nameFile.pdf $dirWork/$nameFile.txt 
+									mv $dirWork/$nameFile.pdf $dirWork/$nameFile.txt  $dirConserves/$dateRep;	
+
+									
 									insertionData  "INSERT INTO compterendubox(path,datecreation,biologistevalidateur,nbrpage,disponible,piedpage,signature,entetepage,certificat,nompatient,numerodossier,sexe,adresse,medecin,email)VALUES
 									('$dirArchives/$dateRep/$nameFile.pdf','$dateInsrt','$bioValideur','$nbPage',false,false,false,false,false,'$nomPatient','$numeroDossier','$sexe','$adressePatient','$medecin','$emailPatient');"
 								fi;				
-					else
-						#ERREUR2
+									
+					fi;
+
+				else 
+					#ERREUR2
 						#Deplacement des fichiers dans le dossier Erreur2 qui n'ont pas le numero dossier et nomPatient
 						mv $dirWork/$nameFile.pdf $dirWork/$nameFile.txt $dirErreur2;
 						#ecriture dans le fichier log du jour
@@ -158,15 +158,13 @@
 						echo $dateProg": "$statut >> $dirLogs/$dateRep/$nomFileLog.txt		
 
 						#Ruperation du type d'erreur et insertion erreur
-						dateInsrt="$(date +%Y-%m-%d' '%H:%M:%S)";		
 						Typerreur=$(selectData "SELECT idtypeerreur FROM typeerreur WHERE typeerreur='ERREUR2';")		
 						if [ -n "$Typerreur" ]; then
 							insertionData "INSERT INTO erreur(typeerreurid,date) VALUES($Typerreur,'$dateInsrt');"
 							echo "Erreur les champs importants ne sont renseignés numeroDossier et nomPatient"
-						fi;
-					fi;
-
-				else 
+						fi;			
+				fi;
+			else
 				#ERREUR1
 				#Deplacement des fichiers dans le dossier Erreur1 qui ne sont pas des comptes rendus
 				mv $dirWork/$nameFile.pdf $dirWork/$nameFile.txt $dirErreur1;
@@ -180,10 +178,8 @@
 				if [ -n "$Typerreur" ]; then
 					insertionData "INSERT INTO erreur(typeerreurid,date) VALUES($Typerreur,'$dateInsrt');"
 					echo "Erreur fichier non compatible"
-				fi;					
-
+				fi;	
 			fi;
-		fi;
 		}
 		#function des selections des données dans la base de donnée
 		selectData(){
@@ -198,36 +194,38 @@
 			if [ "$rq_insert" = "INSERT 0 1" ]; then
 				echo "insertion avec succès"
 			else
+				#ERREUR5
+				#ecriture dans le fichier log du jour
 				statut=$nameFile.pdf"-> Erreur insertion données";
 				echo $dateProg": "$statut >> $dirLogs/$dateRep/$nomFileLog.txt
+
+				#Recuperation du type d'erreur et insertion erreur
+				Typerreur=$(selectData "SELECT idtypeerreur FROM typeerreur WHERE typeerreur='ERREUR5';")		
+				if [ -n "$Typerreur" ]; then
+					insertionData "INSERT INTO erreur(typeerreurid,date) VALUES($Typerreur,'$dateInsrt');"
+					echo "Erreur fichier non compatible"
+				fi;	
+
 			fi;
 		}
 		#-n $chaine Vérifie si la chaîne est non vide.
 		function ajoutCalque(){		
 			#pdftk
 			arg1=$1 #$1 represente first argument
-			if [ -n "$arg1" ]; then #si calque bien ajouter			
-			pdftk $dirWork/$nameFile.pdf stamp $arg1  output $dirArchives/$dateRep/"calque"$nameFile.pdf;
-			echo "calque bien ajouter"
+			if [ -n "$arg1" ]; then #si calque bien ajouter	
+				pdftk $dirWork/$nameFile.pdf stamp  $arg1 output $dirArchives/$dateRep/$nameFile.pdf;
+				echo "calque bien ajouter"
 		    else
 				#ERREUR3
 				statut=$nameFile.pdf"-> Erreur insertion de calques";
 				echo $dateProg": "$statut >> $dirLogs/$dateRep/$nomFileLog.txt
 				rm $dirWork/$nameFile.pdf $dirWork/$nameFile.txt 
 				mv $dirWork/$nameFile.pdf $dirErreur3
-
-
-
-
-
-
-
 				Typerreur=$(selectData "SELECT idtypeerreur FROM typeerreur WHERE typeerreur='ERREUR3';")		
 				if [ -n "$Typerreur" ]; then
 					insertionData "INSERT INTO erreur(typeerreurid,date) VALUES($Typerreur,'$dateInsrt');"
 				fi
 
-				echo "Erreur insertion de calques"
 			fi;
 		}
 
@@ -254,46 +252,8 @@
 
 
 
-		#EXTRACTION DES DONNÉES DANS LE FICHIER TXT
-		extractNumDossier(){
-			echo ""
-		}
-		function extractPatient(){
-			resultat=""
-			
-		}
-
-		function extractAdressePatient(){
-			resultat=""
-			
-		}
-
-		function extractSexePatient(){
-			resultat=""
-			#resultat="" $(grep -E "^Sexe" test.txt | awk {'print $2'})
-			
-		}
-		function extractDateDeNaissance(){
-			resultat=""
-			
-		}
-		function extractEmailPatient(){
-			resultat=""
-
-		}
-		function extractEmailMedecin(){
-			resultat=""
-			
-		}
-		function extractMedecin(){
-			resultat=""
-			
-		}
-		function extractBioValideur(){
-			resultat=""
-			
-		}	
-
+		
+		
 
 		main;
 
